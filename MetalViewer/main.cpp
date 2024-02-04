b9593d7ce8d3df48951b4d0bd68bbd584dcd992c
Hyunwoo Kim <hyunwookimbob@gmail.com>
2024-02-04 20:04:14 +0900

    Initial Commit

---
+++ MetalViewer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MetalViewer
+//
+//  Created by Hyunwoo Kim on 2/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
